840a3620bd3bf1e1d5f321fe105f4d33ef9852c4
Piotr Prostko <piotr.prostko@uhasselt.be>
2021-10-27 12:09:53 +0200

    distribHist

---
+++ distribHist.R
@@ -0,0 +1,8 @@
+distribHist <- function(n, distrib) {
+  arg <- match.arg(distrib, c("normal", "unif"))
+  dat <- switch(arg, "normal" = rnorm(n), "unif" = runif(n))
+  hist(dat)
+}
+
+distribHist(10, "n")
+distribHist(10, "un")
